fc2e73fd302fd3b3154b67f6ed9a7eb8eeb10379
kriti-arora <92243363+kriti-arora@users.noreply.github.com>
2021-10-21 01:20:33 +0530

    Create SnackdownQualifier2021.cpp

---
+++ Codechef/SnackdownQualifier2021.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int T;
+	cin>>T;
+	
+	while(T--){
+		int A,B,C;
+		cin>>A>>B>>C;
+		
+		if(A==7||B==7||C==7){
+			cout<<"YES"<<endl;
+		}
+		else{
+			cout<<"NO"<<endl;
+		}
+ }
+	
+	return 0;
+}
